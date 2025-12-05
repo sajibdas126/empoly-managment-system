85f10505a9550c2d86cf77dc611a79cb15aad829
Sajib <sajibdashtrg@gmail.com>
2025-12-05 18:01:46 +0600

    feat: refine login handling and improve user state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {  useContext, useState } from "react"
+import {  useContext,  useState } from "react"
 import Login from "./components/Auth/Login"
 import AdminDeshbord from "./components/Dashbord/AdminDeshbord"
 import EmployeeDashbord from "./components/Dashbord/EmployeeDashbord"
@@ -10,16 +10,31 @@ import { AuthContext } from "./context/AuthProvider"
 function App() {
 
   const [user,setuser]= useState(null)
+  const [loggedInUserData,setLoggdInUserData]= useState(null)
   const authData = useContext(AuthContext)
+
+  // useEffect(()=>{
+  //   if(authData){
+  //     const loggedInUser = localStorage.getItem("loggedInUser")
+    
+    
+  //   if(loggedInUser){
+  //    setuser(loggedInUser.role)
+  //   }}
+
+  // },[authData])
 
 
   const handleLogin = (email,password) =>{
     if(email == 'admin@gmail.com' && password == '123'){
       setuser('admin');
       console.log(user)
-      // && authData.employess.find((e)=>email == e.email && e.password == password)
+      localStorage.setItem("loggedInUser",JSON.stringify({role:"admin"}))
+      
     }else if(authData){
+      const employess =authData.employess.find((e)=>email == e.email && e.password == password)
      setuser("employess")
+     localStorage.setItem("loggedInUser",JSON.stringify({role:"employess"}))
      console.log(user)
     }else{
       alert("Invalid Credentials")
@@ -36,8 +51,7 @@ function App() {
 
     {user == 'admin' ? <AdminDeshbord></AdminDeshbord>:<EmployeeDashbord></EmployeeDashbord>}
 
-    {/* <EmployeeDashbord></EmployeeDashbord> */}
-    {/* <AdminDeshbord></AdminDeshbord> */}
+    
     </>
   )
 }
